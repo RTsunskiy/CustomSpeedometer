4bfcf468c3745f040b468148077384f96564cddc
RTsunskiy <cunami_27-30@mail.ru>
2019-10-18 15:33:06 +0300

    new

--- app/src/main/java/com/example/customspeedometer/MainActivity.java
+++ app/src/main/java/com/example/customspeedometer/MainActivity.java
@@ -4,15 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SpeedometerProgressView speedometerProgressView;
+    SeekBar speedSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SpeedometerProgressView speedometerProgressView = findViewById(R.id.speedometer_progress_view);
+        speedometerProgressView = findViewById(R.id.speedometer_progress_view);
         speedometerProgressView.setSpeed(150);
         speedometerProgressView.setMaxSpeed(200);
+
+        speedSeekBar = findViewById(R.id.seekBar);
+
+        speedSeekBar.setMax(speedometerProgressView.getMaxSpeed());
+
+        speedSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
     }
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+           speedometerProgressView.setSpeed(progress);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
